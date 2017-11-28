09f970de9b5a827bc2681b7741b9a79d8148c136
jiinyoo <yji3791@gmail.com>
2017-11-28 12:34:14 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,141 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :create_comment, :like_post]
+  before_action :is_login?, only: [:create_comment, :like_post]
+  # GET /posts
+  # GET /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+    @like = true
+    if user_signed_in?
+      @like = current_user.likes.find_by(post_id: @post.id).nil?
+    end
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def create_comment
+    # unless user_signed_in?
+    #   respond_to do |format|
+    #     format.js { render 'please_login.js.erb'}
+    #   end
+    # else
+      # puts params[:body]
+      @c = @post.comments.create(comment_params)
+    # end
+  end
+  
+  def destroy_comment
+    
+    # # unless user_signed_in?
+    # #   respond_to do |format|
+    # #     format.js {render 'please_login.js.erb'}
+    # #   end
+    # # end
+    # puts params[:body]
+    # @c = @post.comments.destroy(body: params[:body])
+  end
+
+
+  def like_post
+    # unless user_signed_in?
+    #   respond_to do |format|
+    #     format.js { render 'please_login.js.erb'}
+    #   end
+    
+    # else
+    if Like.where(user_id: current_user.id, post_id: @post.id ).first.nil?
+      # 라이크라는 테이블에서 첫 번째 로우를 찾았는데 그게 닐 값이니?
+      #좋아요를 누르지 않은 상태에 대한 실행문
+      #좋아요를 만들어주면 된다.
+      @result = current_user.likes.create(post_id: @post.id)
+      puts "좋아요 누름"
+    else
+      #좋아요를 누른 상태에 대한 실행문
+      #기존의 좋아요를 삭제하면 됨.
+      @result= current_user.likes.find_by(post_id: @post.id).destroy
+      puts "좋아요 취소"
+    end
+    # puts "Like Post Success"
+    @result = @result.frozen?
+    #frozen 상태면 dislike버튼으로 바꿔주기
+    # end
+  end
+
+  private
+  
+    def is_login?
+      unless user_signed_in?
+        respond_to do |format|
+          format.js { render 'please_login.js.erb'}
+        end
+      end  
+    end
+    
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :contents)
+    end
+    
+    def comment_params
+      params.require(:comment).permit(:body)
+      #sql인젝션에 대비하기 위해
+      #코멘트에 있는 걸 받아서 바디 파라미터로 받아오기
+    end
+end
